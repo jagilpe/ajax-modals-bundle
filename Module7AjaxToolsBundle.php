159fe1c3ab4162db65821e24ee88a0721218698a
Javier Gil <javier.gil@module-7.com>
2016-09-27 09:19:02 +0200

    Initial commit

---
+++ Module7AjaxToolsBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Module7\AjaxToolsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * This Bundle builds a reusable Ajax Tools
+ *
+ * @author Javier Gil Pereda <javier.gil@module-7.com>
+ */
+class Module7AjaxToolsBundle extends Bundle
+{
+
+}
